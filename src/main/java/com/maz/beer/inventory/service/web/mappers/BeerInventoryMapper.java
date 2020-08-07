8f746f2603622cce60373bdf3d525fe7284ba922
ayoubzoubeidi <ayoubzoubeidi@gmail.com>
2020-08-07 11:08:41 +0100

    intial project

---
+++ src/main/java/com/maz/beer/inventory/service/web/mappers/BeerInventoryMapper.java
@@ -0,0 +1,14 @@
+package com.maz.beer.inventory.service.web.mappers;
+
+import com.maz.beer.inventory.service.domain.BeerInventory;
+import com.maz.beer.inventory.service.web.model.BeerInventoryDto;
+import org.mapstruct.Mapper;
+
+
+@Mapper(uses = {DateMapper.class})
+public interface BeerInventoryMapper {
+
+    BeerInventory beerInventoryDtoToBeerInventory(BeerInventoryDto beerInventoryDTO);
+
+    BeerInventoryDto beerInventoryToBeerInventoryDto(BeerInventory beerInventory);
+}
